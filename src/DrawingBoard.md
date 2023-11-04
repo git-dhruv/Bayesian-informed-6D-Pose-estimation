d75b36e92766578fdbd0cdbb80db5c968bd7d177
Dhruv Parikh <dhruvparikhrg@gmail.com>
2023-11-03 22:00:44 -0400

    initial analysis

--- src/DrawingBoard.md
+++ src/DrawingBoard.md
@@ -1,1 +1,4 @@
 # Pipeline
+## Depth Completition
+
+Depth images are in mm and the maximum value is 3.2 meters. Assume that they go till 4 meters. 
--- src/processDepth.py
+++ src/processDepth.py
@@ -9,6 +9,8 @@ Unsupervised Depth Completion
 
 import sys
 import logging
+import cv2
+import matplotlib.pyplot as plt
 
 class depthCompletion:
     """
@@ -19,9 +21,20 @@ class depthCompletion:
     def __init__(self):
         pass
 
+    def fillDepth(self,img):
+        print(img.max())
+        plt.imshow(img)
+
 
 def main()->None:    
+    img = cv2.imread(r"..\data\0050\depth\000000.png", cv2.IMREAD_UNCHANGED)
     processDepth = depthCompletion()
+    processDepth.fillDepth(img)
+    plt.show(block=False)
+    plt.figure()
+    img = cv2.imread(r"..\data\0050\depth_filled\0000000.png", cv2.IMREAD_UNCHANGED)
+    processDepth.fillDepth(img)
+    plt.show()
 
 if __name__ == "__main__":
     # Configure logging
