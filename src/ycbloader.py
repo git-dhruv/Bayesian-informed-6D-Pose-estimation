9ce3f2abd7df0ff6b436205f0f9ceb336be49800
Dhruv Parikh <dhruvparikhrg@gmail.com>
2023-11-04 11:23:57 -0400

    [tested] video ycb loader

--- src/ycbloader.py
+++ src/ycbloader.py
@@ -19,6 +19,7 @@ from os.path import join as opj
 import glob
 import numpy as np
 from typing import Dict, Any
+import cv2
 
 def handlePath(root, isSynthetic, classId=5, mode=0)-> Dict[str, Any]:
     """
@@ -46,18 +47,20 @@ def handlePath(root, isSynthetic, classId=5, mode=0)-> Dict[str, Any]:
         data['depthA'] = sorted(glob.glob(opj(root, '*depthA.png')))
         data['depthB'] = sorted(glob.glob(opj(root, '*depthB.png')))
         data['npzFiles'] = sorted(glob.glob(opj(root, '*meta.npz')))
+        datalen = len(data['rgbA'])
     else:
         data['rgb'] = sorted(glob.glob(opj(root, 'color/*.png')))
         data['depth'] = sorted(glob.glob(opj(root, 'depth/*.png')))
         data['pose_gt'] = sorted(glob.glob(opj(root, f'pose_gt/{classId}/*.txt')))
         data['K'] = [opj(root, "cam_K.txt")]
+        datalen = len(data['rgb'])
     
     #Sanity Checks for empty keys
     for key, file_list in data.items():
         if len(file_list)==0:
             raise Exception(f"Files for {key} not found! \n Finding in Path: {root}")
-
-    return data #We can handle maxLen here, but then I want to consider all data in shuffle and then cut off :p 
+        
+    return data, datalen #We can handle maxLen here, but then I want to consider all data in shuffle and then cut off :p 
 
 class dataloader(Dataset):
     """
@@ -88,30 +91,69 @@ class dataloader(Dataset):
             Maximum Length of the dataset
         """
         super().__init__()
-        files = handlePath(root, datatype, classId, mode)
-        for key, file_list in files.items():
-            for file_path in file_list[:15]:
-                print(file_path)
-
-
+        self.root = root
+        self.isSynthetic = datatype 
+        self.classId = classId
+        self.mode = mode
+        self.config = config
+        self.datatransform = datatransform
+        self.labeltransform = labeltransform
+        #Get all the files required
+        self.files, datalen = handlePath(root, datatype, classId, mode)
+        print(maxLen)
+        if maxLen is None:
+            maxLen = datalen
+        self.maxLen = maxLen
+        
     def __len__(self):
-        return 0
+        return self.maxLen
 
     def __getitem__(self, idx):
-        pass
+        ### Config, datatransform, labeltransform, files, ply file 
+        if self.isSynthetic:
+            return self.getSynthData(idx)
+        return self.getRealData(idx)
+
+    ### Workhorse of the code begin from below ###
+    def getSynthData(self, idx):        
+        return 1
+
+    def getRealData(self, idx):
+        """
+        Dataloader's job on real data is to not supply the previous image, but to only supply the current image.
+        On real data we need to render synthetic image using openGL render, which requires a lot more arguments, 
+        and thus is avoided in this method to be invoked by a shared call of get_item 
+        """
+
+        rgb = cv2.imread(self.files['rgb'][idx], cv2.IMREAD_UNCHANGED)
+        depth = np.int64(cv2.imread(self.files['depth'][idx], cv2.IMREAD_UNCHANGED))
+        gt = np.loadtxt(self.files['pose_gt'][idx])
+        K = np.loadtxt(self.files['K'][0])     
+        
+        rgb = self.datatransform(rgb)
+        depth = self.datatransform(depth)
+        
+        return rgb, depth, gt, K
+    
+    
 
 
 
 def main()->None:    
-    root = r"data\mustard_bottle"
-    datatype = 1
+    root = r"data\0050"
+    datatype = 0
     classId = 5
     mode = 0
     config = ""
-    datatransform = None
+    datatransform = transforms.ToTensor()
     labeltransform = None
     maxLen = None
     loader = dataloader(root, mode, datatype, config, datatransform, labeltransform, classId, maxLen)
+    train_loader = torch.utils.data.DataLoader(loader, shuffle=False, batch_size=1)
+    for data in train_loader:
+        print(data)
+        break
+
 
 if __name__ == "__main__":
     # Configure logging
