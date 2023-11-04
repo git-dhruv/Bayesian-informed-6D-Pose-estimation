a2b817838c2c3c998ce75567736e787cde550a49
Dhruv Parikh <dhruvparikhrg@gmail.com>
2023-11-04 08:54:24 -0400

    Boilerplate dataloader

---
+++ src/ycbloader.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+ycbloader.py: Dataset for handling YCB data
+"""
+
+import sys
+import logging
+
+class dataloader:
+    """
+    Dataloader for the YCB Data
+    """
+    
+    def __init__(self):
+        """
+        
+        """
+        pass
+
+
+def main()->None:    
+    loader = dataloader()
+
+if __name__ == "__main__":
+    # Configure logging
+    logging.basicConfig(level=logging.INFO)
+    main()
