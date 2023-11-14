87c9bf2e883a03221be48d838beaf5794d914f8f
Dhruv Parikh <dhruvparikhrg@gmail.com>
2023-11-14 10:17:33 -0500

    tested module imports

---
+++ training/train.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+train.py: Training algorithm
+"""
+
+import logging
+
+# Module constants
+# CONSTANT_1 = "value"
+
+# Module "global" variables
+# global_var = None
+
+class train:
+    """
+    
+    """
+    
+    def __init__(self):
+        """
+        Initialize the MyClass instance with attribute1 and attribute2.
+        """
+        pass
+
+
+def main()->None:    
+    pass
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
+
